b25bec6ea5c940f0fa2e50ec962c991c78af89da
Your Name <Phamtn110@gmail.com>
2022-06-07 19:40:04 +0700

    first commit

---
+++ My project (1)/Assets/Enemy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float defend;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.magnitude > defend)
+        { Destroy(gameObject, 0.1f); }
+        else
+        { defend -= collision.relativeVelocity.magnitude; }
+    }	
+}
